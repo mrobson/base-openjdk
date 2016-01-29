a3ab0c1226652ea1dc4b9149cd0f79800caa2d30
Matt Robson <mrobson@redhat.com>
2016-01-29 12:48:23 -0500

    oracle jdk8 with urandom change

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
-DOCKER_IMAGE_NAME=mrobson/base-openjdk
-DOCKER_IMAGE_VERSION=7
+DOCKER_IMAGE_NAME=mrobson/base-oraclejdk
+DOCKER_IMAGE_VERSION=8
 
 docker rmi --force=true ${DOCKER_IMAGE_NAME}:${DOCKER_IMAGE_VERSION}
 docker build --force-rm=true --rm=true -t ${DOCKER_IMAGE_NAME}:${DOCKER_IMAGE_VERSION} .
